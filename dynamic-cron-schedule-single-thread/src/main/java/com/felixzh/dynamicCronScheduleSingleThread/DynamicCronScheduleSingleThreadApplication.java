36d3ccbe7b299fe21f74006132562e48a96ecfe5
felixzh <felixzh>
2021-06-09 16:28:18 +0800

    扩充示例

---
+++ dynamic-cron-schedule-single-thread/src/main/java/com/felixzh/dynamicCronScheduleSingleThread/DynamicCronScheduleSingleThreadApplication.java
@@ -0,0 +1,15 @@
+package com.felixzh.dynamicCronScheduleSingleThread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class DynamicCronScheduleSingleThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicCronScheduleSingleThreadApplication.class, args);
+    }
+
+}
